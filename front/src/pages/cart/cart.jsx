ae5ee048e2db5b27ca4916cd318d298481068da9
VaheKhachtrian <vahe.khachtryan.0806@gmail.com>
2023-12-17 16:48:04 +0400

    first commit

---
+++ front/src/pages/cart/cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Cart = () => {
+    return (
+        <div className='cart'>
+            <div className='shoptitle'>
+                <h1> PedroTech</h1>
+            </div> 
+        </div>
+    )
+}
---
+++ front/src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopTitle'>
+            <h1> PedroTech Shop</h1>
+        </div>
+        {/* <div className='products'>
+
+        </div> */}
+    </div>
+  )
+}
